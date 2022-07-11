e7486af1e096480d4da937c71a17899295dfc041
Khamzaatov <adamkhamzatov9522@gmail.com>
2022-07-11 11:38:33 +0300

    complete

---
+++ src/fakeDatabase.js
@@ -0,0 +1,25 @@
+export const fakeDatabase = {
+    challenges: [
+      {
+        id: 1,
+        name: "99 бутылок",
+        isEasy: false,
+      },
+      {
+        id: 2,
+        name: "шифр интукод",
+        isEasy: true,
+      },
+      {
+        id: 3,
+        name: "бэкэнд для магазина",
+        isEasy: false,
+      },
+      {
+        id: 4,
+        name: "текущий челлендж",
+        isEasy: true,
+      },
+    ],
+  };
+  
